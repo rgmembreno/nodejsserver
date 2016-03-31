d55f9ecc04f8604f0e4f203411811dcf9b0f5d04
Roger Membreno <rgmembreno@gmail.com>
2016-03-31 00:38:26 -0700

    throw out everything

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -1,12 +1,3 @@
 Chef::Log.info("Verifying deployment was successful.")
-serverstart = false
-nodeRunning = 'hello!'
-bash "check_node_is_running" do
-  user "root"
-  code <<-EOH
-      'ps aux | grep node'
-  EOH
-end
-Chef::Log.info("ServerStart #{serverstart}")
-Chef::Log.info("nodeRunning  #{nodeRunning}")
+
 Chef::Log.info("Deployment succeeded")
