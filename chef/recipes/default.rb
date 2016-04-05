daa974bead6cc5473473dd4a21bd8175750da671
Roger Membreno <rgmembreno@gmail.com>
2016-04-05 06:55:52 -0700

    default cookbook override
    it's a shame we need to copy the entire cookbook to change one line in a
    template

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -1,3 +1,12 @@
 Chef::Log.info("Verifying deployment was successful.")
 
+
+http_request 'get local healthCheck' do
+  message ''
+  url 'http://couchdb.apache.org/img/sketch.png'
+  action :get
+  end
+  notifies :create, 'remote_file[/tmp/couch.png]', :immediately
+end
+
 Chef::Log.info("Deployment succeeded")
