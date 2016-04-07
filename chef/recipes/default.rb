b2920310c1f73febf13a8cc62e03e514825bf79e
Roger Membreno <rgmembreno@gmail.com>
2016-04-07 11:02:24 -0700

    different not_if

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -6,7 +6,7 @@ class MyError < StandardError; end
 bash "check_node_is_running" do
   user "root"
   fail MyError, "node is not running"
-  not_if '/usr/bin/pgrep node | wc -l'
+  not_if true
 end
 
 Chef::Log.info("Deployment succeeded")
