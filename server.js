2aaef1f95d13e4e4c674b9a777040c13591d0edf
Roger Membreno <rgmembreno@gmail.com>
2016-04-06 19:11:53 -0700

    try winston logger one last time

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 var express = require('express');
 var app = express();
 var logger = require('winston');
+var expressWinston = require('express-winston');
+var bodyParser = require('body-parser');
 
 var port = 80;
 
-var nconf = require('nconf').argv().env();
+// var nconf = require('nconf').argv().env();
+
+var fileTransportOpts = {
+  filename: './server.log',
+  maxsize: 10000000,
+  maxFiles: 2,
+  json: false,
+  handleExceptions: true
+};
+
+var consoleTransportOpts = {
+  colorize: true,
+  timestamp :true,
+  prettyPrint: true
+};
+
+var fileTransport = new logger.transports.DailyRotateFile(fileTransportOpts);
+var consoleTransport = new logger.transports.Console(consoleTransportOpts);
+
+expressWinston.requestWhitelist.splice(0, expressWinston.requestWhitelist.length);
+expressWinston.requestWhitelist.push('method');
+expressWinston.requestWhitelist.push('url');
+expressWinston.requestWhitelist.push('query');
+
+var message = "{{res.statusCode}} HTTP {{req.method}} {{req.url}} {{res.responseTime}}ms"
+var expressWinstonLogger = expressWinston.logger({
+  transports: [fileTransport, consoleTransport],
+  msg: message,
+  meta: false
+});
+
+var expressWinstonErrorLogger = expressWinston.errorLogger({
+  transports: [fileTransport, consoleTransport],
+  msg: message,
+  meta: false
+});
+
+app.use(bodyParser.json());
+app.use(expressWinstonLogger);
+app.use(expressWinstonErrorLogger);
 
 //routes
 app.get('/', function (req, res) {
