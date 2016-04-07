194f0354a41b279deba01d3e87cd24d5bdfc1b00
Roger Membreno <rgmembreno@gmail.com>
2016-04-07 08:19:09 -0700

    fancy use of wc

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -6,7 +6,7 @@ class MyError < StandardError; end
 bash "check_node_is_running" do
   user "root"
   fail MyError, "node is not running"
-  not_if '/usr/bin/pgrep node'
+  not_if '/usr/bin/pgrep node | wc -l' == 0
 end
 
 Chef::Log.info("Deployment succeeded")
