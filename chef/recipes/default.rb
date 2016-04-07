eeca1d6dac1ab3eaf2548a34fdceb9cde0c91a92
Roger Membreno <rgmembreno@gmail.com>
2016-04-07 07:20:04 -0700

    test test

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -3,9 +3,9 @@ Chef::Log.info("Verifying deployment was successful.")
 
 bash "check_node_is_running" do
   user "root"
-  code <<-EOH
-     /usr/bin/pgrep node
-   EOH
+   fail CustomError, "node is running"
+   only_if '/usr/bin/pgrep node'
+  end
 end
 
 Chef::Log.info("Deployment succeeded")
