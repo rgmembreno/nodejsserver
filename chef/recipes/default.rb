8d84f909b7d32b2a863613c9dfe6ee8cb9cbfcde
Roger Membreno <rgmembreno@gmail.com>
2016-04-07 07:32:50 -0700

    remove extra end

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -3,9 +3,8 @@ Chef::Log.info("Verifying deployment was successful.")
 
 bash "check_node_is_running" do
   user "root"
-   fail CustomError, "node is running"
-   only_if '/usr/bin/pgrep node'
-  end
+  fail CustomError, "node is running"
+  only_if '/usr/bin/pgrep node'
 end
 
 Chef::Log.info("Deployment succeeded")
