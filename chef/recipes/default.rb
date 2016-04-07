ca163889adfa0ecdb8226f10bb4ca321fa150aef
Roger Membreno <rgmembreno@gmail.com>
2016-04-07 11:31:05 -0700

    rewrite

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -2,10 +2,17 @@ Chef::Log.info("Verifying deployment was successful.")
 
 class MyError < StandardError; end
 
+x = false
 
 execute "check_node_is_running" do
-  fail MyError, "node is not running"
-  not_if true
+  user "root"
+  x = true
+  not_if false
 end
 
+Chef::Log.info("Test: #{x}")
+
+if !x
+    fail MyError, "node is not running"
+end
 Chef::Log.info("Deployment succeeded")
