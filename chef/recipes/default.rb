631d9f6bda94bcb66fdd43eb729fbeb43ab0dfa0
Roger Membreno <rgmembreno@gmail.com>
2016-04-07 00:19:20 -0700

    opsworks logs not helpful

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -4,8 +4,8 @@ Chef::Log.info("Verifying deployment was successful.")
 bash "check_node_is_running" do
   user "root"
   code <<-EOH
-    /usr/bin/pgrep node
-  EOH
+     /usr/bin/pgrep node
+   EOH
 end
 
 Chef::Log.info("Deployment succeeded")
