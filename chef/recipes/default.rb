50556218c2b34261f935f84a9ec32cb4ccb60be2
Roger Membreno <rgmembreno@gmail.com>
2016-04-12 20:21:09 -0700

    more proofing

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -8,7 +8,7 @@ ruby_block "check_node_is_running" do
 
       pgrep_command_out = `/usr/bin/pgrep node | wc -l`
       puts pgrep_command_out
-      if pgrep_command_out.stdout == "0"
+      if pgrep_command_out == "0"
           fail MyError, "node is not running"
       end
 end
