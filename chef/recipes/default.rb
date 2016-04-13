8f2671788a749d77d874d94949475654afc0cb0e
Roger Membreno <rgmembreno@gmail.com>
2016-04-12 20:35:18 -0700

    puts again

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -8,7 +8,11 @@ x = false
 ruby_block "check_node_is_running" do
       
       pgrep_command = Mixlib::ShellOut.new("/usr/bin/pgrep node | wc -l")
-  puts pgrep_command.stdout
+      pgrep_command.run_command
+      pgrep_command.error!
+      [pgrep_command.stderr, pgrep_command.stdout].join("\n")
+
+    puts pgrep_command.stdout
       if pgrep_command.stdout == "0"
           fail MyError, "node is not running"
       end
