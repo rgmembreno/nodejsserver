ab39e5d3e4fab72df7e2f31a002b2d9dabadb7c6
Roger Membreno <rgmembreno@gmail.com>
2016-04-12 20:13:42 -0700

    puts instead of shellout

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -1,18 +1,16 @@
 Chef::Log.info("Verifying deployment was successful.")
+
+require 'mixlib/shellout'
 class MyError < StandardError; end
 
-x = false
 
 ruby_block "check_node_is_running" do
-    require 'mixlib/shellout'
 
-    pgrep_command = Chef::Mixin::ShellOut.new("usr/bin/pgrep node | wc -l")
-
-    pgrep_command.run_command
-    puts pgrep_command.stdout
-    if pgrep_command.stdout == "0"
+      pgrep_command_out = `/usr/bin/pgrep node | wc -l`
+      puts pgrep_command_out
+      if pgrep_command_out.stdout == "0"
           fail MyError, "node is not running"
-    end
+      end
 end
 
 
