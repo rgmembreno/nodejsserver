86e47ded4aad628f3c0e97cf1d5723eb9b8b35ae
Roger Membreno <rgmembreno@gmail.com>
2016-04-12 20:33:22 -0700

    mixedlib again

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -3,12 +3,13 @@ Chef::Log.info("Verifying deployment was successful.")
 require 'mixlib/shellout'
 class MyError < StandardError; end
 
+x = false
 
 ruby_block "check_node_is_running" do
-
-      pgrep_command_out = `/usr/bin/pgrep node | wc -l`
-      puts pgrep_command_out
-      if pgrep_command_out == "0"
+      
+      pgrep_command = Mixlib::ShellOut.new("/usr/bin/pgrep node | wc -l")
+  puts pgrep_command.stdout
+      if pgrep_command.stdout == "0"
           fail MyError, "node is not running"
       end
 end
