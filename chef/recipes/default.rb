e07ec4a03d1148ef115f5bb0c7821b464c6cdec3
Roger Membreno <rgmembreno@gmail.com>
2016-04-12 19:58:47 -0700

    mixin still not working

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -1,16 +1,15 @@
 Chef::Log.info("Verifying deployment was successful.")
-
-require 'mixlib/shellout'
 class MyError < StandardError; end
 
 x = false
 
 ruby_block "check_node_is_running" do
-
+    require 'mixlib/shellout'
 
     pgrep_command = Mixlib::ShellOut.new("usr/bin/pgrep node | wc -l")
 
     pgrep_command.run_command
+    puts pgrep_command.stdout
     if pgrep_command.stdout == "0"
           fail MyError, "node is not running"
     end
