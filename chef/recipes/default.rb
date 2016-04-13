e71621102803e7768e6414e9c40bb210efe7894c
Roger Membreno <rgmembreno@gmail.com>
2016-04-12 19:37:59 -0700

    mixins

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -1,17 +1,20 @@
 Chef::Log.info("Verifying deployment was successful.")
 
+require 'mixlib/shellout'
 class MyError < StandardError; end
 
 x = false
 
 ruby_block "check_node_is_running" do
-  not_if '/usr/bin/pgrep node | wc -l'
-   x = true
+
+Chef::Resource::RubyBlock.send(:include,Chef::Mixin::ShellOut)
+
+pgrep_command = '/usr/bin/pgrep node | wc -l'
+      pgrep_command_out = shell_out(pgrep_command)
+      if pgrep_command_out.stdout == "0"
+          fail MyError, "node is not running"
+      end
 end
 
-Chef::Log.info("Test: #{x}")
 
-if !x
-    fail MyError, "node is not running"
-end
 Chef::Log.info("Deployment succeeded")
