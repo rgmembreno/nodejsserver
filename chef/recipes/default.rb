847b723a7dd74f6a6c133c5e1a0ef0fd5c550207
Roger Membreno <rgmembreno@gmail.com>
2016-04-25 00:02:30 -0700

    try custom resource again

--- chef/recipes/default.rb
+++ chef/recipes/default.rb
@@ -1,28 +1,7 @@
 Chef::Log.info("Verifying deployment was successful.")
 
-require 'mixlib/shellout'
-class MyError < StandardError; end
-
-pgrep_command = ""
-pgrep_command << "/usr/bin/pgrep node | wc -l"
-      
-      pgrep_command_out = Mixlib::ShellOut.new("#{pgrep_command}")
-      pgrep_command_out.run_command
-      pgrep_command_out.error!
-
-    puts pgrep_command_out.stdout
-Chef::Log.info("pgrep out char 0 #{pgrep_command_out.stdout[0]}")
-Chef::Log.info("pgrep out char 1 #{pgrep_command_out.stdout[1]}")
-
-falsyTest = (pgrep_command_out.stdout[0] == "0")
-Chef::Log.info("Falsy test: #{falsyTest}")
-falsyTest2 = (pgrep_command_out.stdout.to_s == "0")
-Chef::Log.info("Falsy test 2: #{falsyTest2}")
-     
-      if pgrep_command_out.stdout[0] == "0"
-          fail MyError, "node is not running"
-      end
-
-
+verify_deployment 'check if node is running' do
+  action :start
+end
 
 Chef::Log.info("Deployment succeeded")
---
+++ chef/resources/verify_deployment.rb
@@ -0,0 +1,32 @@
+resource_name verify_deployment
+
+
+load_current_value do
+end
+
+action :start do
+
+    require 'mixlib/shellout'
+    class MyError < StandardError; end
+
+    pgrep_command = ""
+    pgrep_command << "/usr/bin/pgrep node | wc -l"
+        
+    pgrep_command_out = Mixlib::ShellOut.new("#{pgrep_command}")
+    pgrep_command_out.run_command
+    pgrep_command_out.error!
+
+    puts pgrep_command_out.stdout
+    Chef::Log.info("pgrep out char 0 #{pgrep_command_out.stdout[0]}")
+    Chef::Log.info("pgrep out char 1 #{pgrep_command_out.stdout[1]}")
+
+    falsyTest = (pgrep_command_out.stdout[0] == "0")
+    Chef::Log.info("Falsy test: #{falsyTest}")
+    falsyTest2 = (pgrep_command_out.stdout.to_s == "0")
+    Chef::Log.info("Falsy test 2: #{falsyTest2}")
+     
+    if pgrep_command_out.stdout[0] == "0"
+        fail MyError, "node is not running"
+    end
+
+end
