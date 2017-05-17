134a8c7084fd1633f2a47c53da758804a1dbb085
Roger Membreno <rgmembreno@gmail.com>
2017-05-17 12:20:56 -0700

    Revert "another change"
    This reverts commit 650b890635a7232864291a98f7be6b9c3a486fcc.

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(expressWinstonErrorLogger);
 
 //routes
 app.get('/', function (req, res) {
-  res.send('I am doing good again!')
+  res.send('I am doing good!')
 })
 
 app.get('/healthCheck', function (req, res) {
