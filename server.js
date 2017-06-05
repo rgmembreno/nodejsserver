46359eba9ff62e128ed63420c3bc8e30c3eac223
Roger Membreno <rgmembreno@gmail.com>
2017-06-05 00:02:11 -0700

    one quick test change

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var logger = require('winston')
 var expressWinston = require('express-winston')
 var bodyParser = require('body-parser')
 var nconf = require('nconf').argv().env()
+var crypto = require('crypto')
 
 var port = 80
 
@@ -48,8 +49,7 @@ app.use(expressWinstonErrorLogger)
 
 //routes
 app.get('/', function (req, res) {
-  nconf.set('HELLO_WORLD', 'hello world!')
-  res.send('I am doing good, and I want to say ' + nconf.get('THANKS') + ' for ' + (nconf.get('HELLO_WORLD') || 'nothing!'))
+  res.send('I am doing something')
 })
 
 app.get('/healthCheck', function (req, res) {
@@ -58,6 +58,16 @@ app.get('/healthCheck', function (req, res) {
 
 app.get('/envvar', function (req, res) {
   res.send('Environment variable value: ' + process.env.TEST_VAR + "," + process.env.NODE_ENV + "," + process.env.NODE_PROTECTED)
+})
+
+app.get('/verify', function (req, res) {
+  var data = new Buffer('{"id":233320341,"repository":{"id":7903758,"name":"nodejsserver","owner_name":"rgmembreno","url":null},"number":"1","config":{"sudo":false,"language":"node_js","node_js":["0.12.7"],"script":["make -f .travis test"],"notifications":{"webhooks":"http://requestb.in/18uaooo1"},".result":"configured","group":"stable","dist":"precise"},"status":1,"result":1,"status_message":"Failed","result_message":"Failed","started_at":"2017-05-17T17:07:54Z","finished_at":"2017-05-17T17:08:40Z","duration":46,"build_url":"https://travis-ci.org/rgmembreno/nodejsserver/builds/233320341","commit_id":67559042,"commit":"3ddf4bfde685961b835f022a90a882b0d34b6a78","base_commit":null,"head_commit":null,"branch":"master","message":"test build for travis-org","compare_url":"https://github.com/rgmembreno/nodejsserver/compare/f0c3335915a2...3ddf4bfde685","committed_at":"2017-05-17T17:06:56Z","author_name":"Roger Membreno","author_email":"rgmembreno@gmail.com","committer_name":"Roger Membreno","committer_email":"rgmembreno@gmail.com","matrix":[{"id":233320342,"repository_id":7903758,"parent_id":233320341,"number":"1.1","state":"finished","config":{"sudo":false,"language":"node_js","node_js":"0.12.7","script":["make -f .travis test"],"notifications":{"webhooks":"http://requestb.in/18uaooo1"},".result":"configured","group":"stable","dist":"precise","os":"linux"},"status":1,"result":1,"commit":"3ddf4bfde685961b835f022a90a882b0d34b6a78","branch":"master","message":"test build for travis-org","compare_url":"https://github.com/rgmembreno/nodejsserver/compare/f0c3335915a2...3ddf4bfde685","started_at":"2017-05-17T17:07:54Z","finished_at":"2017-05-17T17:08:40Z","committed_at":"2017-05-17T17:06:56Z","author_name":"Roger Membreno","author_email":"rgmembreno@gmail.com","committer_name":"Roger Membreno","committer_email":"rgmembreno@gmail.com","allow_failure":false}],"type":"push","state":"failed","pull_request":false,"pull_request_number":null,"pull_request_title":null,"tag":null}')
+  var key = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvtjdLkS+FP+0fPC09j25\ny/PiuYDDivIT86COVedvlElk99BBYTrqNaJybxjXbIZ1Q6xFNhOY+iTcBr4E1zJu\ntizF3Xi0V9tOuP/M8Wn4Y/1lCWbQKlWrNQuqNBmhovF4K3mDCYswVbpgTmp+JQYu\nBm9QMdieZMNry5s6aiMA9aSjDlNyedvSENYo18F+NYg1J0C0JiPYTxheCb4optr1\n5xNzFKhAkuGs4XTOA5C7Q06GCKtDNf44s/CVE30KODUxBi0MCKaxiXw/yy55zxX2\n/YdGphIyQiA5iO1986ZmZCLLW8udz9uhW5jUr3Jlp9LbmphAC61bVSf4ou2YsJaN\n0QIDAQAB\n-----END PUBLIC KEY-----'
+  var provided = 'UmMMIejjJE1TKc2kSybMdGtR8Y6Z52lGK08v9SfOAIGttLOLIsN4bw3sjaYl43MZP4xIroH91XN9bVUCovlr7CDYonpM2++2wAb6ZEl4Sa96d0Bz2Zyr/7bZXTAEHUeXfEYqFRI8BbD4CjVpTCw7OF/1bYR+Gg7UzE5DrKy1z9iGeNEXcD5yrT4qBvNatFia0m9ZwsEi6KiFAtYCFVEtSVD64D23b8wH/upQ0IUanZSUtf4Ji4qhQxIp2Ah4T2YJJtZRJa8DICsI/KEI3zphKaq0mR7ULDW3GLDmYLITX17kOR2OxDfpgnsLur03xANKx7bQlcXggfIKDc298tmE/w=='
+  var verifier = crypto.createVerify('RSA-SHA1')
+  verifier.update(data)
+  var result = verifier.verify(key, provided, 'base64')
+  res.send('Signature is valid: ' + result)
 })
 
 var server = app.listen(port, function () {
